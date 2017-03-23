6e8fda1825461601be10f8037d8eac7617270c61
SNatraj <natraj_s@yahoo.com>
2017-03-23 09:55:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
 
+@app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
 
@@ -28,15 +28,7 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    #	if req.get("result").get("action") != "food.discovery":
-     #   return {}
-    #result = req.get("result")
-    #parameters = result.get("parameters")
-    #cuisine_type = parameters.get("cuisine_type")
-	#distance = parameters.get("restaurant_distance")
-
-    #speech = "Cuisine is" + cuisine_type + " " + distance
-	    if req.get("result").get("action") != "food.discovery":
+    if req.get("result").get("action") != "food.discovery":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
@@ -45,9 +37,7 @@ def makeWebhookResult(req):
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-	
 
-	
     print("Response:")
     print(speech)
 
@@ -56,13 +46,14 @@ def makeWebhookResult(req):
         "displayText": speech,
         #"data": {},
         # "contextOut": [],
-        "source": "apiai-assistservice"
+        "source": "apiai-onlinestore-shipping"
     }
 
 
-#if __name__ == '__main__':
-#    port = int(os.getenv('PORT', 5000))
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
 
- #   print "Starting app on port %d" % port
+    print "Starting app on port %d" % port
 
-  #  app.run(debug=True, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
+
