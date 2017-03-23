20732cc83913d0df4590fb7239de84f8001a2278
SNatraj <natraj_s@yahoo.com>
2017-03-23 21:26:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,57 +1,8 @@
 #!/usr/bin/env python
 
-import urllib
-import json
-import os
 import mysql.connector
 
-from flask import Flask
-from flask import request
-from flask import make_response
 
-# Flask app should start in global layout
-app = Flask(__name__)
-
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = makeWebhookResult(req)
-
-    res = json.dumps(res, indent=4)
-    print(res)
-	makeinsert(req)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-def makeWebhookResult(req):
-    if req.get("result").get("action") != "food.discovery":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    cuisine = parameters.get("cuisine-type")
-
- 
-
-    speech = "Cuisine is " + cuisine
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "apiai-assistservice1"
-    }
-
-def makeinsert(req):
 	cnx = mysql.connector.connect(user='root', password='Natraj123$',
                               host='localhost',
                               database='assistservice')
@@ -72,11 +23,6 @@ def makeinsert(req):
 
 	cursor.close()
 	cnx.close()
-	return {}
+
 	
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
-
-    app.run(debug=True, port=port, host='0.0.0.0')
