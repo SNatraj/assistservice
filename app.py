1d1a0f08eb51479ad4a58bc77b22da8f1df47e4d
SNatraj <natraj_s@yahoo.com>
2017-03-23 09:50:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import urllib
 import json
 import os
 
-
 from flask import Flask
 from flask import request
 from flask import make_response
@@ -50,9 +49,9 @@ def makeWebhookResult(req):
     }
 
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+#if __name__ == '__main__':
+#    port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
+ #   print "Starting app on port %d" % port
 
-    app.run(debug=True, port=port, host='0.0.0.0')
+  #  app.run(debug=True, port=port, host='0.0.0.0')
