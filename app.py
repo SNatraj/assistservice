9bc5f15b26a16d2b2911099e4bf49163852390c0
SNatraj <natraj_s@yahoo.com>
2017-03-24 10:47:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,35 @@ import urllib
 import json
 import os
 
+import mysql.connector
+from mysql.connector import errorcode
+try:
+  cnx = mysql.connector.connect(host = "localhost",
+                       user = "root",
+                       passwd = "Natraj123$",
+                       db = "assistservice")
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cnx.close()
+  
 from flask import Flask
 from flask import request
 from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
+cnx = mysql.connector.connect(host = "localhost",
+                       user = "root",
+                       passwd = "Natraj123$",
+                       db = "assistservice")
+
+cursor =cnx.cursor()
 
 
 @app.route('/webhook', methods=['POST'])
@@ -34,10 +57,19 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     cuisine = parameters.get("cuisine-type")
 
- 
-
     speech = "Cuisine is " + parameters.get("cuisine-type") + " " + parameters.get("restaurant-distance")
-	
+	add_employee = ("INSERT INTO employees "
+               "(first_name, last_name,  gender) "
+               "VALUES (%s, %s, %s,)")
+
+	data_employee = (parameters.get("cuisine-type"), parameters.get("restaurant-distance"),  'M')
+
+	cursor.execute(add_employee, data_employee)
+
+	cnx.commit()
+
+	cursor.close()
+	cnx.close()
     print("Response:")
     print(speech)
 
