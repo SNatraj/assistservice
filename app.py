b09bb794e20668b54e3936e72cd24f4687952690
SNatraj <natraj_s@yahoo.com>
2017-03-24 11:18:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,21 +5,14 @@ import json
 import os
 
 from flask import Flask
-from flaskext.mysql import MySQL
 from flask import request
 from flask import make_response
-mysql = MySQL()
+
 # Flask app should start in global layout
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Natraj123$'
-app.config['MYSQL_DATABASE_DB'] = 'assistservice'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
 
 
 @app.route('/webhook', methods=['POST'])
-
 def webhook():
     req = request.get_json(silent=True, force=True)
 
@@ -41,20 +34,9 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     cuisine = parameters.get("cuisine-type")
 
+ 
+
     speech = "Cuisine is " + parameters.get("cuisine-type") + " " + parameters.get("restaurant-distance")
-	
-	#add_employee = ("INSERT INTO employee "
-     #          "(first_name, last_name,  gender) "
-      #         "VALUES (%s, %s, %s,)")
-
-	#data_employee = (parameters.get("cuisine-type"), parameters.get("restaurant-distance"),  'M')
-	#cursor = mysql.connect().cursor()
-	#cursor.execute(add_employee, data_employee)
-
-	#connection.commit()
-
-	#cursor.close()
-	#connection.close()
 	
     print("Response:")
     print(speech)
